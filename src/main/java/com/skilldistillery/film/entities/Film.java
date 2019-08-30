938b620a95618894fed54a6fb58249aa293589fb
Carl Lawson <carl.k.lawson@gmail.com>
2019-08-30 16:06:26 -0600

    stubbed out id in film, added controller package and class

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.film.entities;
 
 public class Film {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
